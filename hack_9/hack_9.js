1ea6d0f84865a6385ce0a06fee139542e03e710c
Yotman Reyes <yotman.reyes@gmail.com>
2022-11-22 22:59:08 -0400

    resolve the nith hack

--- hack_9/hack_9.js
+++ hack_9/hack_9.js
@@ -9,6 +9,11 @@
 let arr = ["foo","bar","baz","qux","echo"];
 let result = 0;
 
+for(let i = 0; i < arr.length; i++){
+    if(arr[i][0] == 'b'){
+        result += 1;
+    }
+}
 
 //export result
 module.exports = result;
