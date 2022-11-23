aa0202a84040a4901dd82a1f54c13ee5fc484a00
Yotman Reyes <yotman.reyes@gmail.com>
2022-11-22 22:56:44 -0400

    resolve the third hack

--- hack_3/hack_3_for.js
+++ hack_3/hack_3_for.js
@@ -8,5 +8,9 @@
 let result = [];
 
 
+for(let i = 5; i >= 1; i--){
+    result.push(i);
+}
+
 //export result
 module.exports = result;
--- hack_3/hack_3_while.js
+++ hack_3/hack_3_while.js
@@ -7,6 +7,12 @@
  */
 let result = [];
 
+let i = 5;
+
+while(i >= 1){
+    result.push(i)
+    i--;
+}
 
 //export result
 module.exports = result;
